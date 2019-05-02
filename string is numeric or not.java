9420d022eaf7862b93f63d3708577aabf3cb9e4b
IndhurekhaS <42089681+IndhurekhaS@users.noreply.github.com>
2019-05-02 11:12:00 +0530

    Create string is numeric or not.java

---
+++ string is numeric or not.java
@@ -0,0 +1,17 @@
+ import java.util.*;
+ public class MyClass {
+    public static void main(String args[]) {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        int i=Integer.parseInt(s);
+        if(i>=0)
+        {
+            System.out.println("yes");
+        }
+        else
+        {
+            System.out.println("no");
+        }
+        
+    }
+}
